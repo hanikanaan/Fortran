069d26d0c2c6d8a27759e2961d30e98d276b1e91
hanikanaan <58312140+hanikanaan@users.noreply.github.com>
2021-09-21 10:00:02 -0400

    Add files via upload

---
+++ assignment1.f90
@@ -0,0 +1,9 @@
+program areaOfCircle
+    implicit none
+    REAL(4)::pi, radius, resu
+    pi=4.0 * atan(1.0)
+    radius=72
+    resu=pi*72*72
+    print *, 'Hello, my name is Hani and my student number is 215573272.'
+    print *,  'The area of a 72m-radius circle is:', resu
+end program areaOfCircle
