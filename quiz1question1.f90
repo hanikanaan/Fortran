6510d3514563d6afd21217161cfcd06d536f3748
hanikanaan <58312140+hanikanaan@users.noreply.github.com>
2021-10-04 17:44:57 -0400

    Add files via upload

---
+++ quiz1question1.f90
@@ -0,0 +1,14 @@
+program question1
+    implicit none
+    real::a, y
+    real, dimension(4)::x
+    integer::i
+    x=(/1.3,2.6,3.9,5.2/)
+    do i =  1, 4
+        a = 1 / (3 + (27.1 * (x(i) ** 2)))
+        y = ((x(i)**3.2 - 1) ** (4 * cos(x(i)))) / (log(x(i) - 1) - a)
+        print *, "For  x=", x(i), "y is equal to: ", y
+    end do
+
+
+end program question1
